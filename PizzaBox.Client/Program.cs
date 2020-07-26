cc8682afa1c4a79db50c9e3c5a3429fd447cae80
earl <acearl57@gmail.com>
2020-07-26 17:42:17 -0400

    updated main and added models

--- PizzaBox.Client/Program.cs
+++ PizzaBox.Client/Program.cs
@@ -6,7 +6,60 @@ namespace PizzaBox.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            welcomeBanner();
+            primaryLoop();
+        }
+        static void welcomeBanner()
+        {
+            Console.WriteLine("Welcome to Pizza Box");
+            Console.WriteLine("Where we know you are smart enough to use a command line UI");
+            Console.WriteLine("");
+            Console.WriteLine("Here at Pizza Box we make pizzas you consume.");
+            Console.WriteLine("Follow prompt instructions to complete an order");
+            Console.WriteLine("");
+        }
+        static void displayMainMenu()
+        {
+            Console.WriteLine("Enter a number option then press enter to continue");
+            Console.WriteLine("Would you like to...");
+            Console.WriteLine("A : Add a Pizza");
+            Console.WriteLine("B : Complete your order");
+            Console.WriteLine("Q : To exit the program");
+            Console.WriteLine("");
+        }
+        static void primaryLoop()
+        {
+            Boolean exit = false;
+            string input;
+            while(exit != true)
+            {
+                displayMainMenu();
+                input = acceptInput();
+                if(input.Equals("Q"))
+                {
+
+                }
+            }
+        }
+        static string acceptInput()
+        {
+            string input = "";
+            Console.Write("Enter option here ->");
+            try
+            {
+                input = Char.ConvertFromUtf32(Console.Read()).ToUpper();
+                Console.Write(input);
+                //char.Parse(input);
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                
+            }
+            return input;
         }
     }
 }
